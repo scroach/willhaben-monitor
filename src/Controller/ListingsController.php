2c481ee51aec1e121e53f8c17fc2070565ceab26
Patrick Winkelmayer <4665730+scroach@users.noreply.github.com>
2024-10-05 13:03:52 +0200

    added new scatter chart, use chartjs because apex doesnt handle scatter with thousands of data points very well

--- src/Controller/ListingsController.php
+++ src/Controller/ListingsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Listing;
 use App\Message\DownloadImagesMessage;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,13 +24,29 @@ class ListingsController extends AbstractController
     }
 
     #[Route('/listings/{id}', name: 'details')]
-    public function details(Listing $listing, EntityManagerInterface $em): Response
+    public function details(Listing $listing, EntityManagerInterface $em, LoggerInterface $logger): Response
     {
+        $result = $em->getRepository(Listing::class)->createQueryBuilder('l')
+            ->select('l.priceCurrent, l.area, l.zip, l.id')
+            ->andWhere('l.priceCurrent IS NOT NULL')
+            ->andWhere('l.priceCurrent BETWEEN 1000 AND 1000000')
+            ->andWhere('l.area IS NOT NULL')
+            ->andWhere('l.area BETWEEN 90 AND 500')
+            ->andWhere('l.lastSeen IS NOT NULL')
+            ->addOrderBy('l.zip', 'ASC')
+            ->getQuery()->getResult();
+
+        $data = [];
+        foreach ($result as $row) {
+            $zip = substr($row['zip'], 0, 2).'xx';
+            $data[$zip][] = ['price' => $row['priceCurrent'], 'area' => $row['area'], 'id' => $row['id']];
+        }
+
         $listing->updateAggregatedData();
         $em->flush();
 
 
-        return $this->render('details.twig', ['listing' => $listing]);
+        return $this->render('details.twig', ['listing' => $listing, 'scatterData' => $data]);
     }
 
     #[Route('/listings/{id}/fetch-images', name: 'fetch-images')]
